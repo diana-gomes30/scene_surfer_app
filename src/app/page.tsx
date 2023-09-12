dc0286ddf705502a865d4ef90282a416a33c1308
PCGAMER-DIANA\Diana <diana.fp.gomes@gmail.com>
2023-09-12 17:42:07 +0100

    Add a responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen text-black">
+    <main className="flex flex-col min-h-screen text-white">
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
+      <h1>Hello World!!</h1>
       <h1>Hello World!!</h1>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,74 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 export default function Navbar() {
+  const [isOpen, setisOpen] = useState(false);
+
   return (
-    <header>
-      <ul className="text-red-500">
-        <li>Home</li>
-        <li>TV Shows</li>
-        <li>Movies</li>
-        <li>My List</li>
-      </ul>
-    </header>
+    <div className="bg-black sticky top-0">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-row items-center justify-between w-full md:w-auto">
+          <Link
+            href={'/'}
+            className="flex font-medium items-center text-red-500"
+          >
+            <span>SceneSurfer</span>
+          </Link>
+
+          <div className="block md:hidden">
+            <button
+              onClick={() => setisOpen(!isOpen)}
+              className="flex items-center px-3 py-2 rounded text-white hover:text-gray-300"
+            >
+              <FontAwesomeIcon
+                icon={faBars}
+                className={`fill-current h-5 w-5 ${
+                  isOpen ? 'hidden' : 'block'
+                }`}
+              />
+              <FontAwesomeIcon
+                icon={faXmark}
+                className={`fill-current h-5 w-5 ${
+                  isOpen ? 'block' : 'hidden'
+                }`}
+              />
+            </button>
+          </div>
+        </div>
+
+        <div
+          className={`block flex-grow w-full md:flex md:items-center md:w-auto justify-end ${
+            isOpen ? 'block' : 'hidden'
+          }`}
+        >
+          <Link
+            className="block my-2 md:mr-6 md:inline-block md:my-0 text-white hover:text-gray-300"
+            href={'/'}
+          >
+            Home
+          </Link>
+          <Link
+            className="block my-2 md:mr-6 md:inline-block md:my-0 text-white hover:text-gray-300"
+            href={'/movies'}
+          >
+            Movies
+          </Link>
+          <Link
+            className="block my-2 md:mr-6 md:inline-block md:my-0 text-white hover:text-gray-300"
+            href={'/series'}
+          >
+            Tv Shows
+          </Link>
+          <Link
+            className="block my-2 md:inline-block md:my-0 text-white hover:text-gray-300"
+            href={'/my-list'}
+          >
+            My List
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
